564dba608f4d6cdd60fdb144f1f524b2250b266f
Joshua inyang <joshuainyang255@gmail.com>
2021-04-22 07:41:04 +0100

    another commit

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-from flask import Flask, url_for, request, render_template, redirect
+from config import *
+from flask import url_for, request, render_template, redirect
 from markupsafe import escape
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from models import BlogPost, db
 
-
-#You need to use following line [app Flask(__name__]
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(app)
-
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return 'Blog post' + str(self.id)
 
 
 all_posts = [
