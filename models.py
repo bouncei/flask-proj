f0346f6edee2ee48e516bf9c5a9b08bbb6dd9fcb
Joshua inyang <joshuainyang255@gmail.com>
2021-08-21 21:01:51 +0100

    commit

--- models.py
+++ models.py
@@ -5,13 +5,21 @@ from datetime import datetime
 
 
 # Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Secret key
 app.config['SECRET_KEY'] = 'joshua inyang'
 
 # Datebase
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# app.config["TEMPLATES_AUTO_RELOAD"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+
+
+
+# app.config['SQLALCHEMY_BINDS'] = {
+#     'users' : 'sqlite///users.db',
+# }
+
 db = SQLAlchemy(app)
 
 # Blog Post Model
@@ -21,18 +29,19 @@ class BlogPost(db.Model):
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(20), nullable=False, default='N/A')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))     #A foriegn key column refrencing to the user id
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))     #A foriegn key column refrencing to the user id
     
     def __repr__(self): 
         return 'Blog post' + str(self.id)
 
 
 class User(db.Model, UserMixin):
-    id = db.column(db.Integer, primary_key=True)
+    # __bind_key__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     firstname = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
-    blogs = db.relationship('BlogPost')
+    # blogs = db.relationship('BlogPost')
     
 
